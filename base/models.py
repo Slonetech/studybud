624e184eb1310a89126dbbb1c7f43840182699fb
Slonetech <kengeregordon@gmail.com>
2024-02-12 23:01:13 +0300

    starting-database

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    #host
+    #topic
+    name = models.CharField(max_length=100)
+    description = models.Textfield(null=True,blank=True)
